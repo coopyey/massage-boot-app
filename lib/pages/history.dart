f5373daffc9def11c65ccb10c27ec8b988e3d85a
Coop <mjmohr1@shockers.wichita.edu>
2019-11-07 06:07:10 -0600

    android loading works again
    gradle issues were due to a change in dependency requirements, and the new version of flutter_blue has some weird compatibility issues - owner recommends 0.6.2 at this time despite the exceptions involved

--- lib/pages/history.dart
+++ lib/pages/history.dart
@@ -1,25 +1,90 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+
+class CounterStorage {
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/counter.txt');
+  }
+
+  Future<int> readCounter() async {
+    try {
+      final file = await _localFile;
+
+      // Read the file
+      String contents = await file.readAsString();
+
+      return int.parse(contents);
+    } catch (e) {
+      // If encountering an error, return 0
+      return 0;
+    }
+  }
+
+  Future<File> writeCounter(int counter) async {
+    final file = await _localFile;
+
+    // Write the file
+    return file.writeAsString('$counter');
+  }
+}
 
 class HistoryPage extends StatefulWidget {
+  final CounterStorage storage;
+
+  HistoryPage ({Key key, @required this.storage}) : super(key: key);
+
   @override
   _HistoryPage createState() => _HistoryPage();
 }
 
 class _HistoryPage extends State<HistoryPage> {
-  @override 
+  int _counter;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.storage.readCounter().then((int value) {
+      setState(() {
+        _counter = value;
+      });
+    });
+  }
+
+  Future<File> _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+
+    // Write the variable as a string to the file.
+    return widget.storage.writeCounter(_counter);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar (title: new Text('Historical Data'),),
-      body: new Container(
-        //padding: new EdgeInserts.all(32.0),
-        child: new Center(
-          child: new Column(
-            children: <Widget>[ 
-              const Text('Hello friendo'),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(title: Text('Reading and Writing Files')),
+      body: Center(
+        child: Text(
+          'Button tapped $_counter time${_counter == 1 ? '' : 's'}.',
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
     );
-  } // Widget build
-} // Class _HistoryPage
+  }
+}
