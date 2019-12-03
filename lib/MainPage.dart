e00f8a97bf0da4775bef73de1cc4c189c2b9bd4f
kevin-brink <kbrink170@gmail.com>
2019-12-02 19:42:50 -0600

    Bluetooth sending

--- lib/MainPage.dart
+++ lib/MainPage.dart
@@ -4,10 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'dart:convert';
 
 import './DiscoveryPage.dart';
 
-enum Sequencing {off, low, medium, high}
+enum Sequencing {off, low, medium, high}  // Enumerator for sequence/state
 
 class MainPage extends StatefulWidget {
   final HistoryStorage storage;
@@ -17,7 +18,7 @@ class MainPage extends StatefulWidget {
   _MainPage createState() => new _MainPage();
 }
 
-class HistoryStorage {
+class HistoryStorage {                  // For persistent logs
   Future<String> get _localPath async {
     final directory = await getApplicationDocumentsDirectory();
 
@@ -51,7 +52,7 @@ class HistoryStorage {
 
 
 class _MainPage extends State<MainPage> {
-  BluetoothState _bluetoothState = BluetoothState.UNKNOWN;
+  BluetoothState _bluetoothState = BluetoothState.UNKNOWN;            // Initialize btState
 
   String _address = "...";
   String _name = "...";
@@ -59,17 +60,17 @@ class _MainPage extends State<MainPage> {
   Sequencing _sequence = Sequencing.off; //Starts off by default
   Sequencing _historical; //Data loaded from file
   double _setrate = 0;
-  int _bootpressure = 0; //Pressure readout starts
-  int _setpressure = 0; //Pressure sent to boot starts 0 by default
+  int _bootpressure = 0; //Pressure readout starts                      (To later implement custom
+  int _setpressure = 0; //Pressure sent to boot starts 0 by default       pressure levels I assume)
 
-  Timer _discoverableTimeoutTimer;
+  Timer _discoverableTimeoutTimer;                    // Come back to this, idk what it is
   int _discoverableTimeoutSecondsLeft = 0;
 
   @override
   void initState() {
     super.initState();
 
-    widget.storage.readFile().then((int value) {
+    widget.storage.readFile().then((int value) {      // Load custom default or historical pressure setting from file
       setState(() {
         if (value == 0) {
           _historical = Sequencing.off;
@@ -84,7 +85,8 @@ class _MainPage extends State<MainPage> {
     });
     
     // Get current state
-    FlutterBluetoothSerial.instance.state.then((state) {
+
+    FlutterBluetoothSerial.instance.state.then((state) {        // Checks the phone's bt adapter state
       setState(() { _bluetoothState = state; });
     });
 
@@ -105,7 +107,7 @@ class _MainPage extends State<MainPage> {
     FlutterBluetoothSerial.instance.name.then((name) {
       setState(() { _name = name; });
     });
-
+    
     // Listen for futher state changes
     FlutterBluetoothSerial.instance.onStateChanged().listen((BluetoothState state) {
       setState(() {
@@ -129,6 +131,9 @@ class _MainPage extends State<MainPage> {
     super.dispose();
   }
 
+  BluetoothConnection connection;
+  bool get isConnected => connection != null && connection.isConnected;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -139,7 +144,7 @@ class _MainPage extends State<MainPage> {
         child: ListView(
           children: <Widget>[
             ListTile(
-              title: RaisedButton(
+              title: RaisedButton(      //Discover Devices button
                 child: const Text('Discover Bluetooth Devices'),
                 onPressed: () async {
                   final BluetoothDevice selectedDevice = await Navigator.of(context).push(
@@ -148,6 +153,9 @@ class _MainPage extends State<MainPage> {
 
                   if (selectedDevice != null) {
                     print('Discovery -> selected ' + selectedDevice.address);
+                    BluetoothConnection.toAddress(selectedDevice.address).then((_connection) {      // Attaches 'connection' to the actual device...i think.
+                      connection = _connection;                                                     // Doesnt matter, sending data at least.
+                    });
                   }
                   else {
                     print('Discovery -> no device selected');
@@ -175,6 +183,7 @@ class _MainPage extends State<MainPage> {
                               setState(() {
                                 _sequence = value;
                                 //bluetooth control to send off signal should go here
+                                connection.output.add(utf8.encode("offPos\r\n"));
                               });
                             },
                           ), // Off
@@ -190,6 +199,7 @@ class _MainPage extends State<MainPage> {
                                 _setpressure = value.index;
                                 _updateFile(value.index);
                                 //bluetooth control to send on signal should go here
+                                connection.output.add(utf8.encode("seq1\r\n"));
                               }); 
                             }, 
                           ), // Low
@@ -209,6 +219,7 @@ class _MainPage extends State<MainPage> {
                                 _setpressure = value.index;
                                 _updateFile(value.index);
                                 //bluetooth control to send on signal should go here
+                                connection.output.add(utf8.encode("seq2\r\n"));
                               }); 
                             },
                           ),
@@ -224,6 +235,7 @@ class _MainPage extends State<MainPage> {
                                 _setpressure = value.index;
                                 _updateFile(value.index);
                                 //bluetooth control to send on signal should go here
+                                connection.output.add(utf8.encode("seq3\r\n"));
                               }); 
                             }, 
                           ),
